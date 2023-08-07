9550f47ac28cbba7ac9bef86800f59f4d6ebd457
Manmeet Singh <95163474+manmeet-ms@users.noreply.github.com>
2023-08-07 16:31:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # THIS IS MADE FOR GITHUB ACITONS 
 import time
-import smtplib
 import os
 import discord
 from  random import randint
@@ -11,9 +10,101 @@ import asyncio
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
 f_w=open('BuildConfig.txt', 'w') # file to save f_w to
-
 # My Browser's Header
 HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50', 'Accept-Language': 'en-US, en;q=0.5'})
+async def fetch(component, url):
+    global f_w, lst
+    r = get(url, headers=HEADERS)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    lst=[]
+    name= (soup.find('h1'))
+    lst.append(name.string.strip())
+    table = soup.find('table', class_='text-center ph-table-offer')
+    if table:
+        rows = table.find_all('tr')
+        for row in rows:
+            # th = row.find('th', scope='row')
+            td = row.find('td')
+            for _ in range(2):
+                # if th and td:
+                if td:
+                    # key = th.text.strip()
+                    lst.append(td.text.strip())
+                    # data[key] = value`
+    name =lst[0]
+    price=lst[1]
+    fetched_data= (f"""
+`{name}`
+Price: {price}
+""")
+
+    print(fetched_data)
+    # print(fetched_data)
+    f_w.write(fetched_data)
+
+    # String processing conversion to int 
+    price =price[1:]
+    price=price.replace(',', '')
+    return int(price)
+
+async def main():
+    L = await asyncio.gather(
+            fetch('CPU', 'https://price-history.com/product/amd-5000-series-ryzen-5-5600x-7o5pfhce'),
+            fetch('GPU', 'https://price-history.com/product/asus-dual-radeon-rx-6600-8-gHeLVdmb'),
+            fetch('MOBO', 'https://price-history.com/product/asus-b550-rog-strix-b550-f-afVbQaG5'),
+            fetch('RAM', 'https://price-history.com/product/corsair-vengeance-lpx-16gb-2x8gb-ddr4-p5Jfhhce'),
+            fetch('SSD', 'https://price-history.com/product/samsung-970-evo-plus-1tb-pcie-xDT82vyp'),
+            fetch('PSU', 'https://price-history.com/product/cooler-master-mwe-550-bronze-v2-rBRCfoSj'),
+    )
+    totalSum=0
+    for i in range(5):
+        totalSum+=L[i]
+    f_w.writelines(f"""
+    Total Sum: Rs.{str(totalSum).strip()}
+    """)
+    M = await asyncio.gather(
+                fetch('Archer', 'https://price-history.com/product/archer-tech-lab-astra-m200-mechanical-3NMVBcw8'),
+                # fetch('CB-GK 34', 'https://price-history.com/product/renewed-cosmic-byte-cb-gk-34-LUB7wmce'),
+        )
+    
+    f_w.writelines=(f"""
+via CronJobs - Github Actions
+{time.strftime('%H:%M:%S')}
+""")
+asyncio.run(main())
+
+f_w.close()
+
+
+f_r=open('BuildConfig.txt', 'r')
+# Discord starts
+intent=discord.Intents.default()
+client=discord.Client(intents=intent)
+msg=(f_r.read())
+# print(msg)
+
+@client.event
+async def on_ready():
+    print(f'Logged in As {client.user}')
+    try:
+        await send_message()
+    except Exception as e:
+        print(e)
+    finally:
+        await client.close() # To clso the connection after out job is done
+
+async def send_message():
+    mention='everyone'
+    embed_colors=[0xf8fafc, 0x64748b, 0xfed7aa, 0xf97316, 0xfef3c7, 0xfbbf24, 0xecfccb, 0x22c55e, 0x0d9488, 0x22d3ee, 0x0ea5e9, 0x8b5cf6, 0xf9a8d4, 0xf472b6, 0xfda4af]
+    channel_id=1136610536256192542 #pc-config
+    channel = client.get_channel(channel_id)
+    embedtry=discord.Embed(description=f"@{mention} {msg}", color=embed_colors[randint(0, len(embed_colors)-1)])
+    await channel.send(embed=embedtry)
+
+client.run(DISCORD_TOKEN)
+print("Success!")
+
 
 # async def fetch(component, url):
 #     global f_w
@@ -98,102 +189,3 @@ HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (K
 # print("Success!")
 
 
-
-async def fetch(component, url):
-    global f_w, lst
-
-    r = get(url, headers=HEADERS)
-
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    lst=[]
-    name= (soup.find('h1'))
-    lst.append(name.string.strip())
-    table = soup.find('table', class_='text-center ph-table-offer')
-    if table:
-        rows = table.find_all('tr')
-        for row in rows:
-            # th = row.find('th', scope='row')
-            td = row.find('td')
-            for _ in range(2):
-                # if th and td:
-                if td:
-                    # key = th.text.strip()
-                    lst.append(td.text.strip())
-                    # data[key] = value`
-    name =lst[0]
-    price=lst[1]
-    fetched_data= (f"""
-`{name}`
-Price: {price}
-""")
-
-    print(fetched_data)
-    # print(fetched_data)
-    f_w.write(fetched_data)
-
-    # String processing conversion to int 
-    price =price[1:]
-    price=price.replace(',', '')
-    return int(price)
-
-async def main():
-    L = await asyncio.gather(
-            fetch('CPU', 'https://price-history.com/product/amd-5000-series-ryzen-5-5600x-7o5pfhce'),
-            fetch('GPU', 'https://price-history.com/product/asus-dual-radeon-rx-6600-8-gHeLVdmb'),
-            fetch('MOBO', 'https://price-history.com/product/asus-b550-rog-strix-b550-f-afVbQaG5'),
-            fetch('RAM', 'https://price-history.com/product/corsair-vengeance-lpx-16gb-2x8gb-ddr4-p5Jfhhce'),
-            fetch('SSD', 'https://price-history.com/product/samsung-970-evo-plus-1tb-pcie-xDT82vyp'),
-            fetch('PSU', 'https://price-history.com/product/cooler-master-mwe-550-bronze-v2-rBRCfoSj'),
-    )
-    totalSum=0
-    for i in range(5):
-        totalSum+=L[i]
-
-    # print(totalSum)
-    f_w.writelines(f"""
-Total Sum: Rs.{str(totalSum).strip()}
-""")
-
-    M = await asyncio.gather(
-                fetch('Archer', 'https://price-history.com/product/archer-tech-lab-astra-m200-mechanical-3NMVBcw8'),
-                # fetch('CB-GK 34', 'https://price-history.com/product/renewed-cosmic-byte-cb-gk-34-LUB7wmce'),
-        )
-asyncio.run(main())
-
-f_w.writelines=(f"""
-via CronJobs - Github Actions
-{time.strftime('%H:%M:%S')}
-
-""")
-
-f_w.close()
-
-
-f_r=open('BuildConfig.txt', 'r')
-# Discord starts
-intent=discord.Intents.default()
-client=discord.Client(intents=intent)
-msg=(f_r.read())
-# print(msg)
-
-@client.event
-async def on_ready():
-    print(f'Logged in As {client.user}')
-    try:
-        await send_message()
-    except Exception as e:
-        print(e)
-    finally:
-        await client.close() # To clso the connection after out job is done
-
-async def send_message():
-    mention='everyone'
-    embed_colors=[0xf8fafc, 0x64748b, 0xfed7aa, 0xf97316, 0xfef3c7, 0xfbbf24, 0xecfccb, 0x22c55e, 0x0d9488, 0x22d3ee, 0x0ea5e9, 0x8b5cf6, 0xf9a8d4, 0xf472b6, 0xfda4af]
-    channel_id=1136610536256192542 #pc-config
-    channel = client.get_channel(channel_id)
-    embedtry=discord.Embed(description=f"@{mention} {msg}", color=embed_colors[randint(0, len(embed_colors)-1)])
-    await channel.send(embed=embedtry)
-
-client.run(DISCORD_TOKEN)
-print("Success!")
